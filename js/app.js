c60e07d9f2b65a209ded25d762f11a66c72bdf3c
colpos <thyartis@gmail.com>
2020-01-03 10:51:34 +0100

    Added some functionalities to the editing process of events such as closing the popup window and filling the fields with information of the event you clicked on. Also added a "local Database" as a json file that is loaded when loading the page. Changes like ading new ones or deleting the test Data from the calendar will not be saved to the Database yet.

--- js/app.js
+++ js/app.js
@@ -125,6 +125,10 @@ app.get("/webfonts/fa-solid-900.ttf", (req, res) => {
 	res.sendFile("fa-solid-900.ttf", {root: path.join("./webfonts")});
 });
 
+app.get("/json/events.json", (req, res) => {
+	res.sendFile("events.json", {root: path.join("./json")});
+});
+
 
 const server = http.createServer(app);
 
