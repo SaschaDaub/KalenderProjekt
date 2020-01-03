837a446ee4dc322528804792d2b09d2ff1d44cb8
colpos <thyartis@gmail.com>
2020-01-03 10:54:28 +0100

    gitignore now added with node modules directory

--- js/projektScript.js
+++ js/projektScript.js
@@ -1,14 +1,137 @@
-/*var startDate = [start._d.getMonth().AddZero(),
-				start._d.getDate().AddZero(),
-				start._d.getFullYear()].join('/') + ', ' +
-					[start._d.getHours().AddZero(),
-					start._d.getMinutes().AddZero()].join(':');
 
-				var endDate = [end._d.getMonth().AddZero(),
-						end._d.getDate().AddZero(),
-						end._d.getFullYear()].join('/') + ', ' +
-					[end._d.getHours().AddZero(),
-						end._d.getMinutes().AddZero()].join(':'); */
+var events_array = [];
+var clickedEvent;
+
+$(document).ready(function () {
+    var $confirmButton = $('#btn-confirm');
+    var $modal = $('.modal');
+    var newEvent = {};
+
+    addLocalSavedEvents();
+
+    $('#naviButtons i').click({param1: $(this).parentElement}, navigationClickHandler);
+
+
+    $confirmButton.on('click', function (e) {
+        var usage = $('.modal')[0].attributes.usage.value;
+
+        if (usage === "add") {
+            addNewEvent(newEvent, $modal);
+        } else if (usage === "edit") {
+            editEvent(newEvent, $modal);
+        }
+    });
+});
+
+
+function navigationClickHandler(parent) {
+    $('#mainContent').children().remove();
+    $('#naviButtons li').removeClass('buttonClicked');
+    parent.currentTarget.parentElement.className += 'buttonClicked';
+    $('#currentTabText').text(parent.currentTarget.title);
+
+    if (parent.currentTarget.title === "Calendar") {
+
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+
+        var $modal = $('.modal');
+        var $close = $('.btn-close');
+
+        var calendarEl = $('<div/>').attr('id', 'calendar');
+        $('#mainContent').append(calendarEl);
+        $('#calendar').fullCalendar({
+            plugins: ['interaction', 'dayGrid', 'moment'],
+            height: 'parent',
+            header: {
+                left: 'month agendaWeek agendaDay',
+                center: 'today prev,next',
+                right: 'title'
+            },
+            editable: true,
+            selectable: true,
+            selectHelper: true,
+            eventLimit: true,
+            events: events_array,
+            select: function (start, end) {
+                if ($('#delete-button').length > 0) {
+                    $('#delete-button').remove();
+                }
+                $('.modal').attr('usage', 'add');
+                var windowHeight = $(window).height(),
+                    windowWidth = $(window).width(),
+                    modalWidth = windowWidth / 4;
+
+                $('.modal-header').removeClass('modal-edit');
+
+                $('#newEventTitle')[0].value = '';
+
+                $('.modal-title').text('Add New Event');
+
+                $modal.show();
+
+                $close.on('click', function () {
+                    $('.modal').hide();
+                });
+
+                $('#newEventStartDate')[0].value = moment(start).format();
+                $('#newEventEndDate')[0].value = moment(end).format();
+                $('#newEventColor')[0].value = "#3eb7c9";
+                $('#newEventTextColor')[0].value = "#ffffff";
+            },
+
+            eventDrop: function (event, delta, revertFunc) {
+                var eventArrayId = event.id - 1;
+                events_array[eventArrayId].start = event.start;
+                events_array[eventArrayId].end = event.end;
+
+                $('#calendar').fullCalendar('updateEvent', event);
+            },
+
+            eventClick: function (event, element) {
+                $('.modal').attr('usage', 'edit');
+                $('.modal-title').text('Edit Event: ' + event.title);
+                $('.modal-header').addClass('modal-edit');
+
+                if ($('#delete-button').length === 0) {
+                    var deleteButton = $('<button>').attr('id', 'delete-button')
+                        .text('Delete')
+                        .addClass('btn')
+                        .click(function () {
+                            $('#calendar').fullCalendar('removeEvents', event._id);
+                            events_array.splice(event.id - 1, 1);
+                            $modal.hide();
+                            $(this).remove();
+                        });
+                    $('.modal-footer').append(deleteButton);
+                }
+
+                $close.on('click', function () {
+                    $('.modal').hide();
+                });
+
+                $('#newEventTitle')[0].value = event.title;
+                $('#newEventStartDate')[0].value = moment(event.start).format('YYYY-MM-DD');
+                $('#newEventEndDate')[0].value = moment(event.end).format('YYYY-MM-DD');
+                $('#newEventColor')[0].value = event.color;
+                $('#newEventTextColor')[0].value = event.textColor;
+                clickedEvent = event;
+                $modal.show();
+            }
+        });
+    } else if (parent.currentTarget.title === "Appointments") {
+        var calendarEl = $('<div/>').attr('id', 'calendar');
+        $('#mainContent').append(calendarEl);
+        $('#calendar').fullCalendar({
+            plugins: ['list'],
+            height: 'parent',
+            defaultView: 'listWeek',
+            events: events_array
+        });
+    }
+}
 
 function getNewEventValues(newEvent) {
     var newEventStartTime = $('#newEventStartTime')[0].value;
@@ -69,125 +192,17 @@ function editEvent(newEvent, $modal) {
     $modal.hide();
 }
 
+function addLocalSavedEvents () {
+    $.ajax({
+       type: "GET",
+       url: "./json/events.json",
+       dataType: "json",
+       success: function (data) {
+           var localSavedEventsList = data.events;
 
-var events_array = [];
-var clickedEvent;
-
-$(document).ready(function () {
-    var $confirmButton = $('#btn-confirm');
-    var $modal = $('.modal');
-    var newEvent = {};
-
-    $('#naviButtons i').click({param1: $(this).parentElement}, navigationClickHandler);
-
-
-    $confirmButton.on('click', function (e) {
-        var usage = $('.modal')[0].attributes.usage.value;
-
-        if (usage === "add") {
-            addNewEvent(newEvent, $modal);
-        } else if (usage === "edit") {
-            editEvent(newEvent, $modal);
-        }
+           $.each(localSavedEventsList, function (index, element) {
+               events_array.push(element);
+           });
+       }
     });
-});
-
-
-function navigationClickHandler(parent) {
-    $('#mainContent').children().remove();
-    $('#naviButtons li').removeClass('buttonClicked');
-    parent.currentTarget.parentElement.className += 'buttonClicked';
-    $('#currentTabText').text(parent.currentTarget.title);
-
-    if (parent.currentTarget.title === "Calendar") {
-        var date = new Date();
-        var d = date.getDate();
-        var m = date.getMonth();
-        var y = date.getFullYear();
-
-        var $modal = $('.modal'),
-            $close = $('.btn-close');
-
-        var calendarEl = $('<div/>').attr('id', 'calendar');
-        $('#mainContent').append(calendarEl);
-        $('#calendar').fullCalendar({
-            plugins: ['interaction', 'dayGrid', 'moment'],
-            height: 'parent',
-            header: {
-                left: 'month basicWeek basicDay agendaWeek agendaDay',
-                center: 'today prev,next',
-                right: 'title'
-            },
-            editable: true,
-            selectable: true,
-            selectHelper: true,
-            eventLimit: true,
-            events: events_array,
-            select: function (start, end) {
-                if ($('#delete-button').length > 0) {
-                    $('#delete-button').remove();
-                }
-                $('.modal').attr('usage', 'add');
-                var windowHeight = $(window).height(),
-                    windowWidth = $(window).width(),
-                    modalWidth = windowWidth / 4;
-
-                $('.modal-header').removeClass('modal-edit');
-
-                $('#newEventTitle')[0].value = '';
-
-                $('.modal-title').text('Add New Event');
-
-                $modal.show();
-
-                $close.on('click', function () {
-                    $('.modal').hide();
-                });
-
-                $('#newEventStartDate')[0].value = moment(start).format();
-                $('#newEventEndDate')[0].value = moment(end).format();
-            },
-
-            eventDrop: function (event, delta, revertFunc) {
-                var eventArrayId = event.id - 1;
-                events_array[eventArrayId].start = event.start;
-                events_array[eventArrayId].end = event.end;
-
-                $('#calendar').fullCalendar('updateEvent', event);
-            },
-
-            eventClick: function (event, element) {
-                $('.modal').attr('usage', 'edit');
-                $('.modal-title').text('Edit Event: ' + event.title);
-                $('.modal-header').addClass('modal-edit');
-
-                if ($('#delete-button').length === 0) {
-                    var deleteButton = $('<button>').attr('id', 'delete-button')
-                        .text('Delete')
-                        .addClass('btn')
-                        .click(function () {
-                            $('#calendar').fullCalendar('removeEvents', event._id);
-                            events_array.splice(event.id - 1, 1);
-                            $modal.hide();
-                            $(this).remove();
-                        });
-                    $('.modal-footer').append(deleteButton);
-                }
-
-                $('#newEventStartDate')[0].value = moment(event.start).format('YYYY-MM-DD');
-                $('#newEventEndDate')[0].value = moment(event.end).format('YYYY-MM-DD');
-                clickedEvent = event;
-                $modal.show();
-            }
-        });
-    } else if (parent.currentTarget.title === "Appointments") {
-        var calendarEl = $('<div/>').attr('id', 'calendar');
-        $('#mainContent').append(calendarEl);
-        $('#calendar').fullCalendar({
-            plugins: ['list'],
-            height: 'parent',
-            defaultView: 'listWeek',
-            events: events_array
-        });
-    }
 }
